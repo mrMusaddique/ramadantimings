ffc241980342da5f498b4ce5d214510635b19f94
Musaddique Shaikh <125135256+mrMusaddique@users.noreply.github.com>
2024-02-28 14:53:14 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.querySelector(".darktheme");
+
+
+button.onclick = function(){
+    document.querySelector(".darktheme").classList.toggle("active");
+    document.body.classList.toggle("dark-theme-color");
+}
+
+// alert("ANEES GANRAL STORE SILLOD");
+
